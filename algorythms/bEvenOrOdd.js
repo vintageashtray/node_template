3197051c205c975ea9d6094de340b8179c416212
vintageashtray <vorpalashtray@gmail.com>
2026-06-30 14:17:46 +0100

    Hints added to guessingGameOOP

--- algorythms/bEvenOrOdd.js
+++ algorythms/bEvenOrOdd.js
@@ -3,9 +3,13 @@ import {l, isArgumentValid} from '../lib/utilities.js'
 export function bEvenOrOdd(n,logLevel=3){
     if (!isArgumentValid(n, ['number'])){return}
 
-    if (n % 2 == 0){
+    const EoO = n % 2 == 0;
+
+    if (EoO){
         l.info(` 2: ${n} is Even`, logLevel)
     } else {
         l.info(` 2: ${n} is Odd`, logLevel)
     }
+
+    return EoO ? 'even' : 'odd'
 }
